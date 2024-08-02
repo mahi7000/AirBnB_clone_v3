d675251c322f6dba8762e76291a7db4ed49a2721
Mahi <mahlet7000@gmail.com>
2024-08-02 04:57:59 -0700

    Update count stats'

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,13 +15,13 @@ def get_status():
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
-    """Retrieves number of object Type
-    """
-    stats = {'amenities': storage.count('Amenity'),
-             'cities': storage.count('City'),
-             'places': storage.count('Places'),
-             'reviews': storage.count('Review'),
-             'states': storage.count('State'),
-             'users': storage.couunt('User')
-             }
-    return jsonify(stats)
+    """number of each objects by type"""
+    obj_dict = {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User")
+        }
+    return jsonify(obj_dict)
