762aef389a06a4ee064e75ddc23a3bd52e2153c3
Mahi <mahlet7000@gmail.com>
2024-08-03 05:19:38 -0700

    amenities api

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -31,7 +31,7 @@ def delete_amenity(amenity_id):
     amenity = storage.get("Amenity", amenity_id)
     if amenity is None:
         abort(404)
-    strorage.delete(amenity)
+    amenity.delete()
     storage.save()
     return make_response(jsonify({}), 200)
 
