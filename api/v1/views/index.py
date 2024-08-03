32520c6d9d8a41f018b1a0c7b7c34171b4c9bc7a
Mahi <mahlet7000@gmail.com>
2024-08-03 04:45:59 -0700

    amenities api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -12,21 +12,6 @@ def get_status():
     """ Return status OK in json format for the Route"""
     return jsonify(status='OK')
 
-
-<<<<<<< HEAD
-@app_views.route('/stats', strict_slashes=False)
-def get_stats():
-    """Retrieves number of object Type
-    """
-    stats = {'amenities': storage.count('Amenity'),
-             'cities': storage.count('City'),
-             'places': storage.count('Places'),
-             'reviews': storage.count('Review'),
-             'states': storage.count('State'),
-             'users': storage.couunt('User')
-             }
-    return jsonify(stats)
-=======
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def get_stats():
     """number of each objects by type"""
@@ -39,4 +24,3 @@ def get_stats():
             "users": storage.count("User")
         }
     return jsonify(obj_dict)
->>>>>>> master
