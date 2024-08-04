ad6275564f909c02b4c99367d901115174834b1b
ericvenom99 <prevenakhtar@mail.com>
2024-08-04 06:29:25 +0200

    added a get user method

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -7,6 +7,7 @@ from flask import Flask, abort, jsonify, make_response, request
 from models import storage
 from models.user import User
 
+
 @app_views.route('/users', strict_slashes=False)
 def get_all_users():
     """List of all users
@@ -14,4 +15,13 @@ def get_all_users():
     all_user = storage.all('User').values()
     if all_user:
         return jsonify([user.to_dict() for user in all_user])
-    
+
+
+@app_views.route('/users/<user_id>', strict_slashes=False)
+def get_user(user_id):
+    """gets a particular user"""
+    user = storage.get('User', user_id)
+    if user:
+        return jsonify(user.to_dict())
+    else:
+        return abort(404)
