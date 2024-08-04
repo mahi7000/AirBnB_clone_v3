47c48322aa5acd3bf527c6f64cbd99d47ae3a5fe
Mahi <mahlet7000@gmail.com>
2024-08-04 07:16:28 -0700

    app views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,9 @@ setup Blueprint
 """
 
 from flask import Blueprint
+
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -11,5 +14,3 @@ from api.v1.views.amenities import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.users import *
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
