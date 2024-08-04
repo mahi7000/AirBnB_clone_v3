bee808fdd728e2eed05746895c783eb7339a06f6
ericvenom99 <prevenakhtar@mail.com>
2024-08-04 07:02:07 +0200

    added get all reviews

--- api/v1/views/places.reviews.py
+++ api/v1/views/places.reviews.py
@@ -7,3 +7,12 @@ from api.v1.views import app_views
 from models import storage
 from models.review import Review
 
+@app_views.route('/places/<place_id>/reviews', strict_slashes=False)
+def  get_all_reviews(place_id):
+    """  Method retrieve list of all Reviews"""
+    place = storage.get('Place', place_id)
+    if place:
+        reviews = place.reviews
+        return jsonify([review.to_dict() for review in reviews])
+    else:
+        return abort(404)
