cead4ae039f3714c5a8bd9319d1ac55feac3ee4e
ericvenom99 <prevenakhtar@mail.com>
2024-08-05 21:13:53 +0200

    updated user

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -82,5 +82,5 @@ def update_user(user_id):
         if key not in ['id', 'created_at', 'updated_at']:
             setattr(data, key, value)
 
-    storage.save()
+    storage.save(user)
     return jsonify(user.to_dict()), 200
